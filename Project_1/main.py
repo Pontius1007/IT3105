5bc92164d804ecdadfe33090239580eeb6d748cc
Pontius1007 <embrikeinang@msn.com>
2018-09-26 11:57:43 +0200

    Start of a real main program

---
+++ Project_1/main.py
@@ -0,0 +1,38 @@
+from input_run_handler import *
+
+
+class Ann():
+    # Class for keeping track of the case manager and the models.
+    def set_cman(self, cman):
+        self.cman = cman
+
+    def get_cman(self):
+        return self.cman
+
+    def set_model(self, model):
+        self.model = model
+
+    def get_model(self):
+        return self.model
+
+
+def main():
+    irh = InputRunHandler()
+
+    while True:
+        u_input = input("Enter command: ")
+
+        if u_input == "q":
+            break
+        elif u_input == "h":
+            print("We support the following commands: ")
+            print("Nothing at the moment")
+        else:
+            irh.evaluate_input(u_input)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
