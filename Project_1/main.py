d0c27b0a9cf399358730e62fa97d5fdb04c09dd6
Pontius1007 <embrikeinang@msn.com>
2018-10-02 12:13:12 +0200

    Add help

--- Project_1/main.py
+++ Project_1/main.py
@@ -1,4 +1,10 @@
 from input_run_handler import *
+import sys
+
+from colorama import init
+init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
+from termcolor import cprint
+from pyfiglet import figlet_format
 
 
 class Ann():
@@ -21,14 +27,31 @@ def main():
     irh = InputRunHandler(ann)
 
     while True:
+        cprint(figlet_format('AI is the future', font='doom'),
+               'yellow', attrs=['bold'])
+        print("\n Welcome to our project. Press 'h' for help. \n")
         u_input = input("Enter command: ")
 
         if u_input == "q":
             break
         elif u_input == "h":
-            print("We support the following commands: ")
+            print("\n We support the following commands: ")
             print("1. 'r' or 'run'. This will start running/training. ")
-            print("2. 'load json' or 'lj'. This will load new parameters from a JSON file. ")
+            print("2. 'q' will quit the application or the input question. ")
+            print("3. 'load json' or 'lj'. This will load new parameters from a JSON file. ")
+            print("4. 'predict' or 'p'. This will execute the predict function.")
+            print("5. 'show' or 'plt'. This will take over your cmd and put the graphs in interactive mode")
+            print("6. The list of datasets we support:\n "
+                  "bitcounter \n"
+                  "autoencoder \n"
+                  "parity \n"
+                  "symmetry \n"
+                  "segmentcounter \n"
+                  "yeast \n"
+                  "glass \n"
+                  "wine \n"
+                  "iris \n"
+                  "mnist \n")
         else:
             irh.evaluate_input(u_input)
 
