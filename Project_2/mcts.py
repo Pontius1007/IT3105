9e36c04a33f7c86e3cbc667eaa4d50e401008cf9
Sebastian Olafsson <msemse.sebastian@gmail.com>
2018-10-18 13:56:50 +0200

    test

---
+++ Project_2/mcts.py
@@ -0,0 +1,17 @@
+class Node:
+    def init(self, parent, left, right):
+        self.parent = parent
+        self.left = left
+        self.right = right
+
+def search():
+    return None
+
+def expand():
+    return None
+
+def evaluate():
+    return None
+
+def backpropogate():
+    return None
