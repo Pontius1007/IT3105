0806e536daba8178aabf86d32371e0cce8456742
Sebastian Olafsson <msemse.sebastian@gmail.com>
2018-10-18 18:46:17 +0200

    base files

--- Project_2/mcts.py
+++ Project_2/mcts.py
@@ -1,17 +1,29 @@
-class Node:
-    def init(self, parent, left, right):
-        self.parent = parent
-        self.left = left
-        self.right = right
+from math import *
 
-def search():
+class Node:
+    def init(self, parent, visits):
+        self.parent = parent
+        self.visits = visits
+        self.wins = 0
+
+    def add_child(self, state):
+        child = Node(state, self)
+
+# traverse from root to node using tree policy (UCB shit)
+def search(node):
+    ucb = node.parent.wins/node.parent.visits + 2 * sqrt(log(node.visits)/node.parent.visits)
     return None
 
+# generate some or all states of child states of a parent state
 def expand():
     return None
 
+# estimates value of node using default policy
 def evaluate():
     return None
 
+# pass evaluating of final state up the tree, updating data
 def backpropogate():
     return None
+
+
