a79aa25dafc3a8798a5bbe3f5abd29bd6f69197c
Pontius1007 <embrikeinang@msn.com>
2018-10-22 14:10:06 +0200

    Add Node functions

--- Project_2/mcts.py
+++ Project_2/mcts.py
@@ -1,15 +1,40 @@
 from math import *
+import random
+
 
 class Node:
-    def init(self, parent = None, state = None):
+    def init(self, parent=None, state=None):
         self.parent = parent
         self.child_nodes = []
         self.visits = 1
         self.wins = 0
+        # How many pieces left, number of child nodes etc
         self.state = state
 
-    def add_child(self, state):
-        child = Node(state, self)
+    def get_state(self):
+        return self.state
+
+    def set_state(self, state):
+        self.state = state
+
+    def get_parent(self):
+        return self.parent
+
+    def set_parent(self, parent):
+        self.parent = parent
+
+    def get_child_nodes(self):
+        return self.child_nodes
+
+    def set_child_nodes(self, child_nodes):
+        self.child_nodes = child_nodes
+
+    def get_random_child(self):
+        return self.child_nodes[random.randint(0, len(self.child_nodes))]
+
+    def add_child(self, child_node):
+        self.child_nodes.append(child_node)
+
 
 
 class MCTS:
@@ -21,7 +46,7 @@ class MCTS:
         best_child = None
         highest_ucb = 0
         for child in node.child_nodes:
-            ucb = child.wins/child.visits + 2 * sqrt(log(node.visits)/child.visits)
+            ucb = child.wins / child.visits + 2 * sqrt(log(node.visits) / child.visits)
             if ucb > highest_ucb:
                 best_child = child
         return self.search(best_child)
@@ -40,6 +65,8 @@ class MCTS:
     def backpropogate(self):
         return None
 
+
+# State manager for NIM
 class NimState:
 
     def nim(self, n, k):
@@ -54,4 +81,3 @@ class NimState:
 class Run:
     def run(self):
         return None
-
