681e1e91401b77afdcbe9a6c338c56fb8e8fb8e5
Pontius1007 <embrikeinang@msn.com>
2018-11-06 15:41:43 +0100

    Class for singel hex cell

---
+++ Project_3/hexCell.py
@@ -0,0 +1,13 @@
+class HexCell:
+    def __init__(self):
+        self.value = [0, 0]
+        self.neighbour = []
+
+    def get_value(self):
+        return self.value
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_neighbour(self):
+        return self.neighbour
